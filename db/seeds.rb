c2fe5cef6a8d6026619f7e9a56ad1bbd5873672d
David Vezzani <david_vezzani@yahoo.com>
2015-08-14 11:17:29 -0700

    updates seed information; more consolidated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+lastnames = %w{Jones Smith Perkins Rown}
+
 5.times do
-  FactoryGirl.create(:member, lastname: "Jones")
+  FactoryGirl.create(:member, lastname: lastnames[0])
 end
 
 4.times do
-  FactoryGirl.create(:member, lastname: "Smith")
+  FactoryGirl.create(:member, lastname: lastnames[1])
 end
 
 5.times do
-  FactoryGirl.create(:member, lastname: "Perkins")
+  FactoryGirl.create(:member, lastname: lastnames[2])
 end
 
 3.times do
-  FactoryGirl.create(:member, lastname: "Rowan")
+  FactoryGirl.create(:member, lastname: lastnames[3])
 end
 
 Member.find(4).update(type: "HomeTeacher")
@@ -27,16 +29,6 @@ Member.find(7).update(type: "HomeTeacher")
 Member.find(12).update(type: "HomeTeacher")
 Member.find(16).update(type: "HomeTeacher")
 
-# works
-#
-comp_01 = Companionship.create
-comp_01.home_teachers << HomeTeacher.all[0..1]
-
-comp_02 = Companionship.create
-comp_02.home_teachers << HomeTeacher.all[2..3]
-
-# better
-#
 # create families
 family_01 = Family.new
 family_01.members << Member.all[0...4]
@@ -52,10 +44,16 @@ family_04.members << Member.all[11...16]
 
 [family_01, family_02, family_03, family_04].map(&:save!)
 
-comp_01.families << [family_01, family_02]
-comp_02.families << [family_03, family_04]
-
-[family_01, family_02, family_03, family_04].each do |f|
-  f.surname = f.members.first.lastname
-  f.save!
+# assign family surnames
+Family.all.each.with_index do |f, i|
+  f.update(surname: lastnames[i])
 end
+
+# create companionships
+comp_01 = Companionship.create
+comp_01.home_teachers = HomeTeacher.all[0..1]
+comp_01.families = [family_01, family_02]
+
+comp_02 = Companionship.create
+comp_02.home_teachers = HomeTeacher.all[2..3]
+comp_02.families = [family_03, family_04]
